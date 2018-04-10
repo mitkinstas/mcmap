9113bea755e831fbb721cdff609040fd9ca33000
Mitkin Stas <mitkinstas@yandex-team.ru>
2018-04-10 14:01:59 +0500

    add static to server.js

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 const workSheetsFromBuffer = xlsx.parse(fs.readFileSync(`${__dirname}/data.xlsx`));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
 const filteredAddresses = data => {
     if (!data) {
         return;
