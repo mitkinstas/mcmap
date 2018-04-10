6bb77890aabd231a95e2af39ab5d2fac1c0a3272
Mitkin Stas <mitkinstas@yandex-team.ru>
2018-04-10 14:15:44 +0500

    update package.json

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 const workSheetsFromBuffer = xlsx.parse(fs.readFileSync(`${__dirname}/data.xlsx`));
 
+app.set('port', port);
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
